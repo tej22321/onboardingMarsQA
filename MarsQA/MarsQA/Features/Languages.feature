70a15a1af2f503829bf72832e59b6497e29d68b6
tej22321 <tejaswi.pinisetty@gmail.com>
2024-09-10 22:45:35 +1000

    add languages to the profile page

--- MarsQA/MarsQA/Features/Languages.feature
+++
@@ -1,8 +0,0 @@
-﻿Feature: Languages for a user profile 
-	
-@mytag
-Scenario: Verify user is able to add new Language to the profile
-	Given the user logs into Porject Mars 
-    And the user navigate to Languages under the profile tab
-    When the user add a new Language 
-	Then Verify Language record is created 
--- MarsQA/obj/Debug/net6.0/MarsQA.AssemblyInfo.cs
+++ MarsQA/obj/Debug/net6.0/MarsQA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarsQA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196bc73d5933861bd32f8dc8e7e3e2de46bf6918")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarsQA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarsQA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
